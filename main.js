43c78430d290648f8bfe2d73ef7ee1b843d9abc5
Tasneem <152633167+Tasneem-oda@users.noreply.github.com>
2024-06-18 00:18:14 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+let img = document.querySelector('.img');
+let container = document.querySelector('.container');
+
+function phone(phone){
+    img.src = phone
+}
+function color(color){
+    container.style.background = color
+}
